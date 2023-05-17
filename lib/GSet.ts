8969b8f3334ec6c08275529f9beab8d2521b3301
Kamran Ansari <crossthinking786@gmail.com>
2023-05-17 21:47:52 +0530

    added tests for GSet

--- lib/GSet.ts
+++ lib/GSet.ts
@@ -1,16 +1,36 @@
-export default class GSet {
-  private _values: Set<any>;
+type GSetTypes = string | number | boolean;
 
-  constructor() {
-    this._values = new Set();
+type GSetIterable = Array<GSetTypes> | Set<GSetTypes>;
+
+export default class GSet {
+  private _values: Set<GSetTypes>;
+
+  static difference(A: GSet, B: GSet) {
+    let differenceSet = A._values;
+
+    for (const element of B._values) {
+      if (differenceSet.has(element)) {
+        differenceSet.delete(element);
+      }
+    }
+
+    return differenceSet;
+  }
+
+  constructor(initialSet?: GSetIterable) {
+    this._values = initialSet ? new Set(initialSet) : new Set();
   }
 
   values() {
-    return Array.from(this._values);
+    return this._values;
   }
 
-  add(value: any) {
+  add(value: GSetTypes) {
     this._values.add(value);
+  }
+
+  has(value: GSetTypes) {
+    return this._values.has(value);
   }
 
   merge(other: GSet) {
---
+++ test/lib/GSet.test.ts
@@ -0,0 +1,92 @@
+import GSet from "../../lib/GSet";
+
+describe("GSet initialization", () => {
+  test("Initializes with empty set", () => {
+    const set = new GSet();
+
+    expect(set.values()).toEqual(new Set());
+  });
+
+  test("Initializes with array iterable", () => {
+    const set = new GSet([1, "str", 2, false]);
+
+    expect(set.values()).toEqual(new Set([1, "str", 2, false]));
+  });
+
+  test("Initializes with set iterable", () => {
+    const set = new GSet(new Set([1, "str", 2, false]));
+
+    expect(set.values()).toEqual(new Set([1, "str", 2, false]));
+  });
+});
+
+describe("GSet difference", () => {
+  test("Different items", () => {
+    const s1 = new GSet(["one", "two", "three", 2]);
+    const s2 = new GSet(["1", "two", "three"]);
+
+    const diff = GSet.difference(s1, s2);
+
+    expect(diff).toEqual(new Set(["one", 2]));
+  });
+
+  test("Same items in both", () => {
+    const s1 = new GSet([1, "two", "three"]);
+    const s2 = new GSet([1, "two", "three"]);
+
+    expect(GSet.difference(s1, s2).size).toBe(0);
+  });
+});
+
+describe("GSet addition", () => {
+  test("Elements get added", () => {
+    const set = new GSet();
+
+    set.add("string");
+    set.add(2);
+
+    expect(set.values()).toEqual(new Set(["string", 2]));
+  });
+
+  test("Added element exists", () => {
+    const set = new GSet();
+
+    set.add("hello");
+    set.add("world");
+
+    expect(set.has("world")).toBeTruthy();
+  });
+
+  test("Not added elements do not exist", () => {
+    const set = new GSet();
+
+    set.add("hello");
+    set.add("world");
+
+    expect(set.has(3)).toBeFalsy();
+  });
+});
+
+describe("GSet merging", () => {
+  test("Different items", () => {
+    const s1 = new GSet([1, 2, "hello"]);
+    const s2 = new GSet([4, 3, "no"]);
+
+    s1.merge(s2);
+    s2.merge(s1);
+
+    expect(s1.values()).toEqual(new Set([1, 2, 3, 4, "hello", "no"]));
+    expect(s1.values()).toEqual(new Set([1, 2, 3, 4, "hello", "no"]));
+  });
+
+  test("Same items", () => {
+    const s1 = new GSet([1, 2, "hello"]);
+    const s2 = new GSet([1, 2, "hello"]);
+
+    s1.merge(s2);
+    s2.merge(s1);
+
+    expect(s1.values()).toEqual(new Set([1, 2, "hello"]));
+    expect(s2.values()).toEqual(new Set([1, 2, "hello"]));
+  });
+});
