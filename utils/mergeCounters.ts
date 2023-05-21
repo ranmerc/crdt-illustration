d9f2af5a8699675336b212f2cbe5079348def00c
Kamran Ansari <crossthinking786@gmail.com>
2023-05-21 10:15:16 +0530

    fix type issues in mergeCounters

--- utils/mergeCounters.ts
+++ utils/mergeCounters.ts
@@ -1,6 +1,13 @@
 import GCounter from "@/lib/GCounter";
+import PNCounter from "@/lib/PNCounter";
 
-export default function mergeCounters(counters: GCounter[]) {
+interface MergeableType<T> {
+  merge: (arg: T) => void;
+}
+
+export default function mergeCounters<T extends MergeableType<T>>(
+  counters: T[]
+) {
   for (let i = 0; i < counters.length; i++) {
     for (let j = 0; j < counters.length; j++) {
       if (i !== j) {
